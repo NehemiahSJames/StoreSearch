a8229db095ba7c8f8ee2e158105d5d5866e34ea5
Nehemiah James <njames7@live.spcollege.edu>
2023-09-18 16:12:41 -0400

    Add a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Nehemiah James on 9/18/23.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
