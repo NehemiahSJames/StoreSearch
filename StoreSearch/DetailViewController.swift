15741d68bb508e60df2912f8bc15915c0de67af1
Nehemiah James <njames7@live.spcollege.edu>
2023-09-26 10:58:29 -0400

    Chapter 38 Completion Commit

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -36,6 +36,17 @@ class DetailViewController: UIViewController {
           updateUI()
         }
         
+        // Gradient view
+        view.backgroundColor = UIColor.clear
+        let dimmingView = GradientView(frame: CGRect.zero)
+        dimmingView.frame = view.bounds
+        view.insertSubview(dimmingView, at: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+      transitioningDelegate = self
     }
     
     deinit {
@@ -96,4 +107,22 @@ extension DetailViewController: UIGestureRecognizerDelegate {
   ) -> Bool {
     return (touch.view === self.view)
   }
+}
+
+extension DetailViewController:
+UIViewControllerTransitioningDelegate {
+  func animationController(
+    forPresented presented: UIViewController,
+    presenting: UIViewController,
+    source: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return BounceAnimationController()
+  }
+    
+    func animationController(
+      forDismissed dismissed: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
+      return SlideOutAnimationController()
+    }
+    
 }
